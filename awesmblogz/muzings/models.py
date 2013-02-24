436db3c992aa80fdc5d9a7dbf4df1ecd734b3a0d
Praveen Gollakota <pgollakota@gmail.com>
2013-02-24 13:56:13 -0500

    Added the Entry model

--- awesmblogz/muzings/models.py
+++ awesmblogz/muzings/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.utils import timezone
 
-# Create your models here.
+
+class Entry(models.Model):
+    STATUS_CHOICES = ((0, 'draft'),
+                      (1, 'hidden'),
+                      (2, 'published'),
+                      )
+    title = models.CharField('title', max_length=255)
+    slug = models.SlugField(blank=True)
+    content = models.TextField('content')
+    status = models.IntegerField(choices=STATUS_CHOICES, default=0)
+    date_created = models.DateTimeField('creation date', default=timezone.now)
+    date_updated = models.DateTimeField('last update', auto_now=True)
+
+    def __unicode__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)[:50]
+        super(Entry, self).save(*args, **kwargs)
